66248868759a4ac50da14ea104914abb38b98c22
Evgeni Golov <evgeni@golov.de>
2025-04-11 08:51:50 +0200

    allow managing /etc/alloy/config.alloy

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,27 @@
 # @summary Install and configure Grafana Alloy
+#
+# @param config
+#   The contents of the configuration file, if any
 class grafana_alloy (
+  Optional[String[1]] $config = undef,
 ) {
   require grafana_alloy::repo
   package { 'alloy': }
-  -> service { 'alloy':
-    ensure => 'running',
-    enable => true,
+
+  if $config {
+    file { '/etc/alloy/config.alloy':
+      content => $config,
+      mode    => '0640',
+      owner   => 'root',
+      group   => 'alloy',
+      require => Package['alloy'],
+      notify  => Service['alloy'],
+    }
+  }
+
+  service { 'alloy':
+    ensure  => 'running',
+    enable  => true,
+    require => Package['alloy'],
   }
 }
