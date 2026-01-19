13e1a7f898e9c0b8cee70ca38bb900538dc3d290
Alexander Fisher <alex@linfratech.co.uk>
2026-01-19 10:27:06 +0000

    Add `version` parameter

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,10 +2,15 @@
 #
 # @param config
 #   The contents of the configuration file, if any
-# @param manage_package_repo Installs the package repositories
+# @param manage_package_repo
+#   Installs the package repositories
+# @param version
+#   The specific version of the `alloy` package to install.
+#   This can be a distro specific package version string, or a string such as `latest` etc.
 class grafana_alloy (
   Optional[Variant[String[1], Sensitive[String[1]]]] $config = undef,
   Boolean $manage_package_repo = true,
+  String[1] $version = 'installed',
 ) {
   if $grafana_alloy::manage_package_repo {
     contain grafana_alloy::repo
--- spec/classes/grafana_alloy_spec.rb
+++ spec/classes/grafana_alloy_spec.rb
@@ -11,9 +11,10 @@ describe 'grafana_alloy' do
       context 'with default params' do
         it { is_expected.to compile.with_all_deps }
         it { is_expected.not_to contain_file('/etc/alloy/config.alloy') }
+        it { is_expected.to contain_package('alloy').with_ensure('installed') }
       end
 
-      context 'with all params' do
+      context 'with `config` param' do
         let(:params) { super().merge(config: 'my config') }
 
         it { is_expected.to compile.with_all_deps }
@@ -32,6 +33,12 @@ describe 'grafana_alloy' do
 
         it { is_expected.to compile.with_all_deps }
         it { is_expected.to contain_file('/etc/alloy/config.alloy').with_content(sensitive('my config')) }
+      end
+
+      context 'with `version` param' do
+        let(:params) { super().merge(version: 'latest') }
+
+        it { is_expected.to contain_package('alloy').with_ensure('latest') }
       end
     end
   end
